071db3d9c6bd841ba29a4d9d0352124f5a4fa488
NyH1409 <hei.nyhasina@gmail.com>
2024-02-09 22:28:51 +0300

    feat: create user

--- src/main/java/com/pjlegacy/base/endpoint/rest/controller/AuthenticationController.java
+++ src/main/java/com/pjlegacy/base/endpoint/rest/controller/AuthenticationController.java
@@ -2,7 +2,9 @@ package com.pjlegacy.base.endpoint.rest.controller;
 
 import com.pjlegacy.base.endpoint.rest.mapper.UserMapper;
 import com.pjlegacy.base.endpoint.rest.model.AuthenticationPayload;
+import com.pjlegacy.base.endpoint.rest.model.SignUp;
 import com.pjlegacy.base.endpoint.rest.model.User;
+import com.pjlegacy.base.model.validator.UserValidator;
 import com.pjlegacy.base.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,11 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
   private final UserService userService;
   private final UserMapper userMapper;
+  private final UserValidator validator;
 
   @PostMapping(value = "/signin")
   public User signin(@RequestBody AuthenticationPayload payload) {
     var authUser = userService.findByFirebaseIdAndEmail(payload.getProviderId(), payload.getEmail());
     return userMapper.toRest(authUser);
+  }
+
+  @PostMapping(value = "/signup")
+  public User signup(@RequestBody SignUp payload){
+    var toSave = userMapper.toDomain(payload);
+    validator.accept(toSave);
+    return userMapper.toRest(userService.save(toSave));
   }
 
 
--- src/main/java/com/pjlegacy/base/endpoint/rest/mapper/UserMapper.java
+++ src/main/java/com/pjlegacy/base/endpoint/rest/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.pjlegacy.base.endpoint.rest.mapper;
 
+import com.pjlegacy.base.endpoint.rest.model.SignUp;
 import com.pjlegacy.base.endpoint.rest.model.User;
 import org.springframework.stereotype.Component;
 
@@ -18,5 +19,29 @@ public class UserMapper {
         .photo(domain.getPhoto())
         .username(domain.getUsername())
         .sex(domain.getSex());
+  }
+
+  public com.pjlegacy.base.model.User toDomain(SignUp domain) {
+    var sex = domain.getSex();
+    return com.pjlegacy.base.model.User.builder()
+        .id(domain.getId())
+        .bio(domain.getBio())
+        .email(domain.getEmail())
+        .about(domain.getAbout())
+        .firstname(domain.getFirstName())
+        .lastname(domain.getLastName())
+        //.birthDate(from(domain.getBirthdate()))
+        .photo(domain.getPhoto())
+        .username(domain.getUsername())
+        .sex(sex !=null ? sexMapper(sex) : null)
+        .build();
+  }
+
+  private static User.SexEnum sexMapper(SignUp.SexEnum sexEnum){
+    return switch (sexEnum){
+      case M -> User.SexEnum.M;
+      case F -> User.SexEnum.F;
+      case OTHER -> User.SexEnum.OTHER;
+    };
   }
 }
