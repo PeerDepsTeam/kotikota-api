c6140750ef82ebd3a8e7f0cb0885e99f77875865
NyH1409 <hei.nyhasina@gmail.com>
2024-02-09 21:38:44 +0300

    feat: exception handler

---
+++ src/main/java/com/pjlegacy/base/model/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.pjlegacy.base.model.exception;
+
+public class NotFoundException extends ApiException {
+  public NotFoundException(String message) {
+    super(ExceptionType.CLIENT_EXCEPTION, message);
+  }
+}
