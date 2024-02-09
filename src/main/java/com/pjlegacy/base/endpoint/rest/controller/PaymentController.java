0fafc9180eda1f6964e91367f0db0ef490f2593b
NyH1409 <hei.nyhasina@gmail.com>
2024-02-10 00:58:47 +0300

    feat: get post funds raised

--- src/main/java/com/pjlegacy/base/endpoint/rest/controller/PaymentController.java
+++ src/main/java/com/pjlegacy/base/endpoint/rest/controller/PaymentController.java
@@ -1,10 +1,14 @@
 package com.pjlegacy.base.endpoint.rest.controller;
 
 import com.pjlegacy.base.endpoint.rest.mapper.PaymentMapper;
+import com.pjlegacy.base.endpoint.rest.model.FundsRaised;
 import com.pjlegacy.base.endpoint.rest.model.PaymentRequest;
 import com.pjlegacy.base.model.validator.PaymentValidator;
 import com.pjlegacy.base.service.PaymentService;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +25,11 @@ public class PaymentController {
     var toSave = mapper.toDomain(toCreate);
     validator.accept(toSave);
     return mapper.toRest(service.save(toSave));
+  }
+
+  @GetMapping(value = "/posts/{pid}/fundsraised")
+  public FundsRaised getFundsRaised(@PathVariable(name = "pid") String pId) {
+    List<com.pjlegacy.base.model.PaymentRequest> payments = service.findByPostId(pId);
+    return mapper.toRestFundsRaised(pId, payments);
   }
 }
--- src/main/java/com/pjlegacy/base/endpoint/rest/mapper/PaymentMapper.java
+++ src/main/java/com/pjlegacy/base/endpoint/rest/mapper/PaymentMapper.java
@@ -1,18 +1,22 @@
 package com.pjlegacy.base.endpoint.rest.mapper;
 
+import com.pjlegacy.base.endpoint.rest.model.FundsRaised;
 import com.pjlegacy.base.endpoint.rest.model.PaymentRequest;
 import com.pjlegacy.base.service.PostService;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import static java.math.BigDecimal.valueOf;
+import static java.util.UUID.randomUUID;
 
 @Component
 @AllArgsConstructor
 public class PaymentMapper {
   private final UserMapper userMapper;
-  private final CategoryMapper categoryMapper;
   private final PostService postService;
 
-  public PaymentRequest toRest(com.pjlegacy.base.model.PaymentRequest domain){
+  public PaymentRequest toRest(com.pjlegacy.base.model.PaymentRequest domain) {
     return new PaymentRequest()
         .id(domain.getId())
         .postId(domain.getPost().getId())
@@ -25,7 +29,7 @@ public class PaymentMapper {
         .reference(domain.getReference());
   }
 
-  public com.pjlegacy.base.model.PaymentRequest toDomain(PaymentRequest rest){
+  public com.pjlegacy.base.model.PaymentRequest toDomain(PaymentRequest rest) {
     var post = postService.getPostById(rest.getPostId());
     return com.pjlegacy.base.model.PaymentRequest.builder()
         .id(rest.getId())
@@ -38,5 +42,16 @@ public class PaymentMapper {
         .label(rest.getLabel())
         .reference(rest.getReference())
         .build();
+  }
+
+  public FundsRaised toRestFundsRaised(String postId, List<com.pjlegacy.base.model.PaymentRequest> payments) {
+    Integer amount = payments.stream().map(com.pjlegacy.base.model.PaymentRequest::getAmount)
+        .reduce(Integer::sum).orElse(0);
+    return new FundsRaised()
+        .id(randomUUID().toString())
+        .amount(valueOf(amount))
+        .postId(postId)
+        .transactions(payments.stream()
+            .map(this::toRest).toList());
   }
 }
