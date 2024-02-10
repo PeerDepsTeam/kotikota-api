d5c45150b26e839ebb5561c009f815a4c7bc3d97
Ylan <hei.nalisoa@gmail.com>
2024-02-10 09:26:00 +0300

    feat: mapper message

---
+++ src/main/java/com/pjlegacy/base/endpoint/rest/mapper/MessageMapper.java
@@ -0,0 +1,41 @@
+package com.pjlegacy.base.endpoint.rest.mapper;
+
+
+import com.pjlegacy.base.model.User;
+import com.pjlegacy.base.model.chatModel.MessageModel;
+import com.pjlegacy.base.model.chatModel.MessageRest;
+import com.pjlegacy.base.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@AllArgsConstructor
+public class MessageMapper {
+
+    private final UserRepository userRepository;
+
+    public MessageModel toRest(Optional<MessageModel> model){
+
+        return model.isPresent() ? MessageModel.builder()
+                .message(model.get().getMessage())
+                .messageDate(model.get().getMessageDate())
+                .id(model.get().getId())
+                .receiver(model.get().getReceiver())
+                .sender(model.get().getSender())
+                .build() : new MessageModel();
+    }
+
+    public MessageModel toModel(MessageRest rest){
+        User sender = userRepository.getById(rest.getIdSender());
+        User receiver = userRepository.getById(rest.getIdReceiver());
+
+        return MessageModel.builder()
+                .sender(sender)
+                .message(rest.getMessage())
+                .receiver(receiver)
+                .id(rest.getId())
+                .build();
+    }
+}
